52275e436d3e0ed623265098b488497aeda2523b
iam-mynuddin <mynuddin.shaik234@gmail.com>
2024-02-18 09:31:28 +0530

    Feb 18

--- ShoppingApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ShoppingApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -107,7 +107,14 @@ namespace ShoppingApp.Areas.Identity.Pages.Account
             public string ConfirmPassword { get; set; }
             public string? Role { get; set; }
             public IEnumerable<SelectListItem> RoleList{ get; set; }
-        }
+            [Required]
+            public string Name { get; set; }
+            public string PhoneNumber { get; set; }
+            public string StreetAddress { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string PostalCode { get; set; }        
+    }
 
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -151,6 +158,12 @@ namespace ShoppingApp.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Name = Input.Name;
+                user.PhoneNumber= Input.PhoneNumber;
+                user.StreetAddress= Input.StreetAddress;
+                user.City= Input.City;
+                user.PostalCode= Input.PostalCode;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
